be719d28a73658216b8e5d397c6f08eeae46f3d2
Asma-Bmdn <60253637+Asma-Bmdn@users.noreply.github.com>
2024-02-20 21:07:26 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,16 +19,16 @@ resource "azurerm_network_security_group" "nsg" {
 
 # Créer une base de données Azure SQL
 resource "azurerm_sql_server" "sql_server" {
-  name                         = "asma8-sql-server"
+  name                         = "asma81-sql-server"
   resource_group_name          = azurerm_resource_group.RG.name
   location                     = azurerm_resource_group.RG.location
   version                      = "12.0"
   administrator_login          = "sqladmin"
-  administrator_login_password = "P@ssw0rd"  # Remplacez par votre mot de passe sécurisé
+  administrator_login_password = "P@ssw0rd" 
 }
 
 resource "azurerm_sql_database" "sql_db" {
-  name                        = "my-sql-database8"
+  name                        = "my-sql-database81"
   resource_group_name         = azurerm_resource_group.RG.name
   location                    = azurerm_resource_group.RG.location
   server_name                 = azurerm_sql_server.sql_server.name
